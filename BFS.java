767f1d01d58a746dd75df2b541e260327f5fef8a
Sarah Kim <sak084@ucsd.edu>
2026-08-17 23:06:17 -0700

    added neighbor calculation and basic loop condition in BFS

--- BFS.java
+++ BFS.java
@@ -20,9 +20,56 @@ public class BFS {
 
         char[][] maze = grid.getMaze();
         char start = maze[startRow][startCol];
+        toVisit.offer(new Point(startRow, startCol));
+    
+        // todo: check visited
+        while (found == false && !toVisit.isEmpty()) {
+            int startRowUp = startRow-1;
+            int startRowDown = startRow+1;
+            int startColLeft = startCol-1;
+            int startColRight = startCol+1;
 
+            // search order: up, down, left, right
+            if (0<=startRowUp && startRowUp<grid.getRowCount() && maze[startRowUp][startCol]!= 'X') {
+                if (maze[startRowUp][startCol] == 'E') {
+                    System.out.println("end point found successfully");
+                    found = true;
+                    return shortestPath;
+                }
+                toVisit.offer(new Point(startRowUp, startCol));
+            }
+            if (0<=startRowDown && startRowDown<grid.getRowCount() && maze[startRowDown][startCol]!= 'X') {
+                if (maze[startRowDown][startCol] == 'E') {
+                    System.out.println("end point found successfully");
+                    found = true;
+                    return shortestPath;
+                }
+                toVisit.offer(new Point(startRowDown, startCol));
+            }
+            if (0<=startColLeft && startColLeft<grid.getColCount() && maze[startRow][startColLeft]!= 'X') {
+                if (maze[startRow][startColLeft] == 'E') {
+                    System.out.println("end point found successfully");
+                    found = true;
+                    return shortestPath;
+                }
+                toVisit.offer(new Point (startRow, startColLeft));
+            }
+            if (0<=startColRight && startColRight<grid.getColCount() && maze[startRow][startColRight]!= 'X') {
+                if (maze[startRow][startColRight] == 'E') {
+                    System.out.println("end point found successfully");
+                    found = true;
+                    return shortestPath;
+                }
+                toVisit.offer(new Point (startRow, startColRight));
+            }
+
+            startPoint = toVisit.poll();
+            visited.offer(startPoint);
+            startRow = startPoint.getRow();
+            startCol = startPoint.getCol();
+        }
         
-        // search order: up, down, left, right
+        //to do
         return shortestPath;
 
     }
